1ede15a6a764db93e25ad756312b6b262d3f54f7
Hung Ngo <hungngo@dayoneteams.com>
2025-08-10 21:20:39 +0700

    feat: update

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <div>
-      <h1 class="text-3xl font-bold underline text-red-500">Hello World</h1>
-    </div>
+    <NuxtLoadingIndicator />
+    <NuxtPage />
   </div>
 </template>
---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <div class="text-3xl font-bold underline text-red-500">Hello World</div>
+  </div>
+</template>
