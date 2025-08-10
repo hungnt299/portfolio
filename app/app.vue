4f5d4756db8573e662b34dfca3d747a00189f5ba
Hung Ngo <hungngo@dayoneteams.com>
2025-08-10 21:00:13 +0700

    feat: update

--- app/app.vue
+++ app/app.vue
@@ -2,7 +2,7 @@
   <div>
     <NuxtRouteAnnouncer />
     <div>
-      <h1 class="text-3xl font-bold underline">Hello World</h1>
+      <h1 class="text-3xl font-bold underline text-red-500">Hello World</h1>
     </div>
   </div>
 </template>
